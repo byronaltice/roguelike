0ffc6d4eac564d70a6932edfb9a8df96addc2a83
byronaltice <byronaltice@gmail.com>
2014-07-27 00:30:05 -0400

    Access violation?

--- roguelike/main.cpp
+++ roguelike/main.cpp
@@ -3,23 +3,35 @@
 #include <iostream>
 using namespace std;
 
-bool IsStringHasUniqueCharacters(string);
+void ReverseString(char*);
+void swap(char*, char*);
 int _tmain(int argc, _TCHAR* argv[])
 {
-	string strString = "0000@";
-	if (IsStringHasUniqueCharacters(strString)) cout << "Yep";
-	else cout << "Nope";
+	char* strString = "hey sup yall beep bop. & asdf yall.";
+	ReverseString(strString);
+	cout << strString;
 	return 0;
 }
 
-bool
-IsStringHasUniqueCharacters(string strString)
+void ReverseString(char* strString)
 {
-	bool aboolCharCheck[500] = { false };
-	for (int i = 0; i < strString.length(); i++)
+	int size = 0;
+	int i;
+	for (i = 0; strString[i] != '\0'; i++)
 	{
-		if (aboolCharCheck[strString[i]]) return false;
-		aboolCharCheck[strString[i]] = true;
+		size++;
 	}
-	return true;
+	if (size == 0) return;
+	for (i = 0; i <= size / 2; i++)
+	{
+		swap(&strString[i], &strString[(size-i)-1]);
+	}
+
+}
+void swap(char* a, char* b)
+{
+	char t = a[0];
+	a[0] = 1;
+	b[0];
+	b[0] = t;
 }
