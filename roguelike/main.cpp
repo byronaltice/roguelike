82ff695ab283844d189e5984bb79acb395b316c5
byronaltice <byronaltice@gmail.com>
2014-07-27 01:28:45 -0400

    Can't modify a string literal, char* was initialized to a string literal

--- roguelike/main.cpp
+++ roguelike/main.cpp
@@ -7,8 +7,27 @@ void ReverseString(char*);
 void swap(char*, char*);
 int _tmain(int argc, _TCHAR* argv[])
 {
-	char* strString = "hey sup yall beep bop. & asdf yall.";
+	char* strString = new char;
+	strString[0] = 'h';
+	strString[1] = 'e';
+	strString[2] = 'e';
+	strString[3] = 'y';
+	strString[4] = ' ';
+	strString[5] = 's';
+	strString[6] = 'u';
+	strString[7] = 'p';
+	strString[8] = ' ';
+	strString[9] = 'y';
+	strString[10] = 'a';
+	strString[11] = 'l';
+	strString[12] = 'l';
+	strString[13] = '.';
+	strString[14] = 'N';
+	strString[15] = '\0';
 	ReverseString(strString);
+	for (int i = 0; strString[i] != '\0'; i++)
+		cout << strString[i];
+	cout << endl;
 	cout << strString;
 	return 0;
 }
@@ -22,7 +41,7 @@ void ReverseString(char* strString)
 		size++;
 	}
 	if (size == 0) return;
-	for (i = 0; i <= size / 2; i++)
+	for (i = 0; i < size / 2; i++)
 	{
 		swap(&strString[i], &strString[(size-i)-1]);
 	}
@@ -31,7 +50,6 @@ void ReverseString(char* strString)
 void swap(char* a, char* b)
 {
 	char t = a[0];
-	a[0] = 1;
-	b[0];
+	a[0] = b[0];
 	b[0] = t;
 }
