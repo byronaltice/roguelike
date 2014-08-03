c458532fb3d4c4af89c321fe491cb07593b7262f
byronaltice <byronaltice@gmail.com>
2014-08-03 15:37:48 -0400

    CardDeck

--- roguelike/main.cpp
+++ roguelike/main.cpp
@@ -1,5 +1,5 @@
 #include "stdafx.h"
-
+#include <deque>
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
@@ -7,3 +7,41 @@ int _tmain(int argc, _TCHAR* argv[])
 	
 	return 0;
 }
+
+template<class T>
+class CardDeck
+{
+public:
+	CardDeck(){}
+	
+	virtual ~CardDeck(){}
+	deque<Card<T>> GetCardDeck() { return stdCardDeque; }
+
+private:
+
+	deque<Card<T>> stdCardDeckDeque;
+};
+template<class T>
+class Card
+{
+public:
+	Card(){}
+	virtual ~Card(){}
+
+	T& GetProperties()
+	{
+		return CardPropertiesType;
+	}
+private:
+	T CardPropertiesType;
+
+};
+
+class Properties
+{
+public:
+	int value;
+	char suit;
+
+private:
+};
